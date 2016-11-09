88fd8530354537039df8ce0190b3771be7913301
Gabriel Boucher <gab.boucher91@gmail.com>
2016-11-08 22:56:23 -0500

    Added classes and methods for display and comm

--- main.py
+++ main.py
@@ -10,52 +10,86 @@
 #Import modules needed
 import serial as sr
 import tkinter as tk
-
+import re
 #Class definition for the things to display and communicate
 
-class controllerPID():
+
+class controllerPIDdisplay():                              #PID controller to display only
     def __init__(self, kp=1, ki=0.001, kd=0.05):
         self.__kp = kp
         self.__ki = ki
         self.__kd = kd
+
     def changeKpValue(self,value):
         self.__kp = value
+
     def changeKiValue(self,value):
         self.__ki = value
+
     def changeKdValue(self,value):
         self.__kd = value
+
     def printKValues(self):
         print(self.__kp, self.__ki, self.__kd)
 
-class defAxis():
+    def extractValues(self,stringToScan):
+        extracted_stuff = re.findall(r"[-+]?\d*\.\d+|\d+",stringToScan)
+
+class controllerPIDsend(controllerPIDdisplay):              #PID controller values to send
+
+    def sendNewData(self):
+        #Method to send stuff via serial
+        a=1
+
+class defAxis():                                            #Points to send through serial
     def __init__(self):
         self.__Points = [None]*768
+
     def addPoint(self,position,value):
         self.__Points[position] = value
+
     def clearPoints(self):
         for i in range(len(self.__Points)):
             self.__Points[i] = None
 
+    def sendPTS(self):
+        #Method to send stuff via serial
+        a=1
 
 
+class dataAcquisition():                                    #Here's the data recieved by the serial port from SD card
+    def __init__(self):
+        self.__data = [[None]*7]
+        self.__position = 0
+
+    def extractValues(self,stringToScan):
+        extracted_stuff = re.findall(r"[-+]?\d*\.\d+|\d+",stringToScan)
+        self.__data += extracted_stuff
+
+    def clearData(self):
+        slef.__data=[[None]*7]
 
 #Class definition for the application
+
+
 class Application(tk.Frame):
-    def __init(self,parent):
+    def __init__(self,parent):
         tk.Frame.__init__(self, parent, background="white")
-        self.parent=parent
+        self.parent = parent
         self.initUI()
+
     def initUI(self):
         self.parent.title("PRoPat Command Software")
-        self.pack(fill=tk.BOTH,expand=1)
+        self.pack(fill=tk.BOTH, expand=1)
+
 
 def mainWindow():
-    root=tk.Tk()
-    root.geometry("500x300+300+300")
-    app=Application(root)
+    root = tk.Tk()
+    root.geometry("1000x500+200+200")
+    app = Application(root)
     root.mainloop()
 
 
 
-
+#Main program
 mainWindow()
